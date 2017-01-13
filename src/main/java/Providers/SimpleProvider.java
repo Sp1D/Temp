22839db85746b3e54d1963179996aa86594b8b57
Evgenii Subbotin <Evgeniy.Subbotin@aplana.com>
2017-01-13 14:50:27 +0500

    Dao added

--- src/main/java/Providers/SimpleProvider.java
+++ src/main/java/Providers/SimpleProvider.java
@@ -1,4 +1,4 @@
-package Providers;
+package providers;
 
 
 public class SimpleProvider {
--- src/main/java/temp/T.java
+++ src/main/java/temp/T.java
@@ -1,8 +1,12 @@
 package temp;
 
 
+import dao.SimpleDao;
+
 public class T {
     void m1() {
-        System.out.println("m1");
+        System.out.println(getClass().getSimpleName() + ": m1");
+        SimpleDao dao = new SimpleDao();
+        dao.read();
     }
 }
