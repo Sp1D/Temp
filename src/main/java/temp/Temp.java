2713247757038fa62d087fe44acda9827c6b2221
Evgenii Subbotin <Evgeniy.Subbotin@aplana.com>
2017-01-13 14:59:46 +0500

    new hello

--- src/main/java/temp/Temp.java
+++ src/main/java/temp/Temp.java
@@ -4,6 +4,7 @@ public class Temp {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello world #2!");
+        System.out.println("Hello world #3!");
         T t = new T();
         t.m1();
     }
