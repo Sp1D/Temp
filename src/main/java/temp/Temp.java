c4e327161b30050ed3bb849235992f5358d94008
Evgenii Subbotin <Evgeniy.Subbotin@aplana.com>
2017-01-13 14:51:25 +0500

    Added new hello

--- src/main/java/temp/Temp.java
+++ src/main/java/temp/Temp.java
@@ -3,6 +3,7 @@ package temp;
 public class Temp {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hello world #2!");
         T t = new T();
         t.m1();
     }
